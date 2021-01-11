4d88bc17f3acec819578c551cec8b0e6c03bb9f2
Pranay Begwani <14PBegwani@patesgs.org>
2021-01-11 16:18:40 +0000

    not much

--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage(response):
-    return render(response, 'HomePage.html', {})
+    return render(response, 'main/HomePage.html', {})
     
