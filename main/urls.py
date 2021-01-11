2d4901096b51440eb45cdf160d814198f1d933e2
Pranay Begwani <14PBegwani@patesgs.org>
2021-01-11 16:15:27 +0000

    Testing Django on the homepage

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views # . is from 'this app' import views
 
 urlpatterns = [
-     path("", views.homepage, name="home"), #name must be same as the function name in the views.py, the path('') in the urls.py of mysite looks for the same path in '' and then renders the page which follows that
+     path("home/", views.homepage, name="home"), #name must be same as the function name in the views.py, the path('') in the urls.py of mysite looks for the same path in '' and then renders the page which follows that
 ]
