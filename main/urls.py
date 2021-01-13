91a42f4d1435d02bf230a8e122ffa37baa7df1c0
Pranay Begwani <14PBegwani@patesgs.org>
2021-01-13 00:19:28 +0000

    Add rough template: add&edit prac- FIXED linking of buttons

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
      path("", views.homepage, name="home"), #name must be same as the function name in the views.py, the path('') in the urls.py of mysite looks for the same path in '' and then renders the page which follows that
      path("LossReport/", views.report_loss, name="loss_report_page"),
      path("AddInventory/", views.add_to_inventory, name="add_to_inventory"),
+     path("AddPractical/", views.add_new_practical, name="add_new_practical"),
+      path("EditPractical/", views.edit_practical, name="edit_practical"),
 ]
--- main/views.py
+++ main/views.py
@@ -10,3 +10,9 @@ def report_loss(response):
     
 def add_to_inventory(response):
     return render(response, 'main/AddToInventory.html', {})
+
+def add_new_practical(response):
+    return render(response, 'main/AddNewPractical.html', {})
+
+def edit_practical(response):
+    return render(response, 'main/EditPractical.html', {})
