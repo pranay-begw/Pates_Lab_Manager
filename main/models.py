d28e90e481f0c1b00be7b1f3d89332a58a7d8ffc
Pranay Begwani <14PBegwani@patesgs.org>
2021-01-19 23:38:58 +0000

    Setting up inventory class model & its attributes

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Inventory_Equipment(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=False, default="Name this Equipment")
+    total_quantity = models.IntegerField(null=False)
+    location = models.CharField(max_length=20, default='Physics Office')
+    img_reference = models.ImageField(null=True)
+
+    class Meta:
+        db_table="inventory"
--- physics_db/settings.py
+++ physics_db/settings.py
@@ -76,11 +76,17 @@ WSGI_APPLICATION = 'physics_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'physics laboratory',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD':'',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
