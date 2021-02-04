74afc437b0e1f4607f26fe02b43674d5e4a98462
Pranay Begwani <14PBegwani@patesgs.org>
2021-02-04 15:45:32 +0000

    Create select pract. dropdown & some comments

--- inventory/views.py
+++ inventory/views.py
@@ -9,17 +9,24 @@ def homepage(response):
 
 def report_loss(response):
     return render(response, 'main/LossOrBreakReport.html', {})
-    
-def add_to_inventory(response):
-    if response.method == "POST":
-        form = Add_Inventory_Form(response.POST) #holds all the information from our form. When submit is clicked this gets a dictionary of all attributes and inputs, creates a new form with all values you entered.
+
+def add_new_to_inventory(request):
+    practical_name = Inventory_Equipment.objects.all()
+    if request.method == "POST":
+        form = Add_Inventory_Form(request.POST) #holds all the information from our form. When submit is clicked this gets a dictionary of all attributes and inputs, creates a new form with all values you entered.
         if form.is_valid():
             form.save()
             # link to a page which shows the full table inventory
     else: #basically if method is GET
         form = Add_Inventory_Form()
 
-    return render(response, 'main/AddToInventory.html', {})
+    return render(request, 'main/AddToInventory.html', {'form': form, 'practical_names': practical_name})
+
+
+###################################################################################################################################
+    # in the above code, retrieve the record of dropdown selection and add the quantity_new value to exisitng qty 
+####################################################################################################################################
+
 
 def add_new_practical(response):
     return render(response, 'main/AddNewPractical.html', {})
