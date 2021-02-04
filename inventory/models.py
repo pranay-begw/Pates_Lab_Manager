b8edfadb293bc146dbef1cf6fcdd7f34007f7be7
Pranay Begwani <14PBegwani@patesgs.org>
2021-02-04 15:45:32 +0000

    Create select pract. dropdown & some comments

--- inventory/models.py
+++ inventory/models.py
@@ -8,19 +8,39 @@ Room_Num = (
     ('Room 5', 'Room 5'),
 )
 
+#blank = True : Not Required
+#null = True : Stores some value as NULL if left empty
 
 class Inventory_Equipment(models.Model):
     id = models.IntegerField(primary_key=True, null=False, default=0)
     name = models.CharField(max_length=255, null=False, default="Name this Equipment")
     total_quantity = models.IntegerField(null=False)
+    quantity_new = models.IntegerField(null=True)
     location = models.CharField(max_length=20, default='Physics Office', choices=Room_Num)
     img_reference = models.ImageField(null=True, blank=True)
 
     class Meta:
         db_table="inventory"
 
+    def __str__(self):
+        return self.name
 
 # Need to figure out auto increase id
 # Need to figure out image upload to db
 # Show table when changes made
 # remove equipment
+
+# less modularizing the app - making it complicated - add all tables in here and link them properly - one by one though
+# first complete inventory
+
+# class Practical(models.Model):
+#     pass
+
+# class Lessons(models.Model):
+#     pass
+
+# class Rooms(models.Model):
+#     pass
+
+# class Staff(models.Model):
+#     pass
