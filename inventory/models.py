2891b1cf21332208948e70eb3a2d6295db651b75
Pranay Begwani <14PBegwani@patesgs.org>
2021-02-07 12:15:15 +0000

    Adds to exisitng equipment qty in inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 
-Room_Num = (
-    ('Room 1', 'Room 1'),
-    ('Room 2', 'Room 2'),
-    ('Room 3', 'Room 3'),
-    ('Room 4', 'Room 4'),
-    ('Room 5', 'Room 5'),
-)
+# Room_Num = (
+#     ('Select', 'Select'),
+#     ('Room 1', 'Room 1'),
+#     ('Room 2', 'Room 2'),
+#     ('Room 3', 'Room 3'),
+#     ('Room 4', 'Room 4'),
+#     ('Room 5', 'Room 5'),
+# )
 
 #blank = True : Not Required
 #null = True : Stores some value as NULL if left empty
 
+# I HAVE MADE THEM ALL BLANK=TRUE -- AFTER ADDING WORKS TRY TO MAKE THEM BLANK CONDITIONALLY SUCH THAT IT IS ALLOWED ONLY IF SELECTING AN EQUIPMENT THAT EXISTS
 
 # the count of the field below must be equal to the count of columns in db table - therefore deleting quantity_ne
 class Inventory_Equipment(models.Model):
-    id = models.IntegerField(primary_key=True, null=False, default=0)
-    name = models.CharField(max_length=255, null=False, default="Name this Equipment") # might have to divide this into name from frop down and new name
-    total_quantity = models.IntegerField(null=True) # qty to add an entirely new equipment - bottom of form
-    location = models.CharField(max_length=20, default='Physics Office', choices=Room_Num)
+    id = models.IntegerField(primary_key=True, null=False, blank=True)
+    name = models.CharField(max_length=255, blank=True, null=True, default="Name this Equipment") # might have to divide this into name from frop down and new name
+    total_quantity = models.IntegerField(null=True, blank=True) # qty to add an entirely new equipment - bottom of form
+    location = models.CharField(max_length=20, default='Physics Office', blank=True)
     img_reference = models.ImageField(null=True, blank=True)
 
     class Meta:
