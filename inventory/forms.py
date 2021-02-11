0f5f1bc62732fadd10ea3c6afb187d27596b6c49
Pranay Begwani <14PBegwani@patesgs.org>
2021-02-11 19:23:09 +0000

    Page to report loss of inventory works!

--- inventory/forms.py
+++ inventory/forms.py
@@ -8,6 +8,14 @@ class Add_Inventory_Form(forms.ModelForm):
         model = Inventory_Equipment
         fields = "__all__"
 
+class Remove_Inventory_Form(forms.ModelForm):
+    quantity_to_remove = forms.IntegerField(required = False, widget=forms.HiddenInput(), initial=0)
+    equipment_name = forms.CharField(max_length=255, required = False)
+    class Meta:
+        model = Inventory_Equipment
+        fields = "__all__"
+
+
     # #add validation function
     # def clean_field(self):
     #     print ('hola from validation')
