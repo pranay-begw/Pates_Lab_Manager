7f246bcbdf1b1c913a199b6b14b7bdd5b9f7f887
Pranay Begwani <14PBegwani@patesgs.org>
2021-02-14 17:05:40 +0000

    changing var name+some editing code in views.py

--- inventory/views.py
+++ inventory/views.py
@@ -8,23 +8,23 @@ def homepage(response):
     return render(response, 'main/HomePage.html', {})
 
 def report_loss(request):
-    practical_name = Inventory_Equipment.objects.all()
+    equipment_name = Inventory_Equipment.objects.all()
     if request.method == "POST":
         form = Remove_Inventory_Form(request.POST) #holds all the information from our form. When submit is clicked this gets a dictionary of all attributes and inputs, creates a new form with all values you entered.
         if form.is_valid():
             if (form.cleaned_data.get("quantity_to_remove") is not None): # use is not null instead
-                practical_name_selected = form.cleaned_data.get("equipment_name")
-                exisitng_quantity = Inventory_Equipment.objects.filter(name = practical_name_selected).values('total_quantity')[0]['total_quantity']
+                equipment_name_selected = form.cleaned_data.get("equipment_name")
+                exisitng_quantity = Inventory_Equipment.objects.filter(name = equipment_name_selected).values('total_quantity')[0]['total_quantity']
                 new_quantity = exisitng_quantity - form.cleaned_data.get("quantity_to_remove")
-                Inventory_Equipment.objects.filter(name = practical_name_selected).values('total_quantity').update(total_quantity = new_quantity)
+                Inventory_Equipment.objects.filter(name = equipment_name_selected).values('total_quantity').update(total_quantity = new_quantity)
             return redirect('/ViewInventory')
             # link to a page which shows the full table inventory
     else: #basically if method is GET
         form = Remove_Inventory_Form()      
-    return render(request, 'main/LossOrBreakReport.html', {'form': form, 'practical_names': practical_name})
+    return render(request, 'main/LossOrBreakReport.html', {'form': form, 'equipment_names': equipment_name})
 
 def add_new_to_inventory(request):
-    practical_name = Inventory_Equipment.objects.all()
+    equipment_name = Inventory_Equipment.objects.all()
     if request.method == "POST":
         form = Add_Inventory_Form(request.POST, request.FILES) #holds all the information from our form. When submit is clicked this gets a dictionary of all attributes and inputs, creates a new form with all values you entered.
         if form.is_valid():
@@ -32,12 +32,12 @@ def add_new_to_inventory(request):
             # CODE SUCH THAT THE FIRST PART OF THIS FORM IS NOT REQUIRED WHEN ADDIN TOTALLY NEW STUFF AND VICE VERSA
 
             if (form.cleaned_data.get("new_quantity") is not None): # use is not null instead
-                practical_name_selected = form.cleaned_data.get("existing_name")
-                print(practical_name_selected)
-                exisitng_quantity = Inventory_Equipment.objects.filter(name = practical_name_selected).values('total_quantity')[0]['total_quantity']
+                equipment_name_selected = form.cleaned_data.get("existing_name")
+                print(equipment_name_selected)
+                exisitng_quantity = Inventory_Equipment.objects.filter(name = equipment_name_selected).values('total_quantity')[0]['total_quantity']
                 print(exisitng_quantity)
                 new_quantity = exisitng_quantity + form.cleaned_data.get("new_quantity")
-                Inventory_Equipment.objects.filter(name = practical_name_selected).values('total_quantity').update(total_quantity = new_quantity)
+                Inventory_Equipment.objects.filter(name = equipment_name_selected).values('total_quantity').update(total_quantity = new_quantity)
                 print (new_quantity)
             #add else here and check
             else:
@@ -46,7 +46,7 @@ def add_new_to_inventory(request):
             # link to a page which shows the full table inventory
     else: #basically if method is GET
         form = Add_Inventory_Form()
-    return render(request, 'main/AddToInventory.html', {'form': form, 'practical_names': practical_name})
+    return render(request, 'main/AddToInventory.html', {'form': form, 'equipment_names': equipment_name})
 
 
 ###################################################################################################################################
@@ -57,18 +57,21 @@ def view_inventory(request):
     inventory_obj = Inventory_Equipment.objects.all()
     return render(request,"main/ViewInventory.html", {'inventory_obj': inventory_obj})
     
-def edit_inventory(request, name):
-    inventory_obj = Inventory_Equipment.objects.get(name = name)
+def edit_inventory(request, id):
+    inventory_obj = Inventory_Equipment.objects.get(id = id)
     return render(request,'main/EditInventoryDetails.html', {'inventory_obj': inventory_obj})
 
-# the file in this case is causing an issue in saving!
-def update(request, name):  
-    inventory_obj = Inventory_Equipment.objects.get(name = name)  
-    form = Add_Inventory_Form(request.POST, request.FILES, instance = inventory_obj)
-    if form.is_valid():
-        form.save()
+# NEED to NOT create a new record if saved. try the update() method?
+def update(request, id):  
+    inventory_obj = Inventory_Equipment.objects.get(id = id)  
+    if (request.method == 'POST'):
+        form = Add_Inventory_Form(request.POST, request.FILES, instance = inventory_obj)
+        if form.is_valid():
+            form.save()
         return redirect("/ViewInventory")
-    return render(request, 'main/EditInventoryDetails.html', {'form': form, 'inventory_obj': inventory_obj })
+    else: #basically if method is GET
+        form = Remove_Inventory_Form()
+    return render(request, 'main/EditInventoryDetails.html', {'inventory_obj': inventory_obj })
 
 def add_new_practical(response):
     return render(response, 'main/AddNewPractical.html', {})
