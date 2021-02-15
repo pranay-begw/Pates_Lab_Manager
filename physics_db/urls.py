a6e3daa3e23b664bf4c0d1399ef0427c449e6501
Pranay Begwani <14PBegwani@patesgs.org>
2021-02-15 20:53:27 +0000

    image redirect fixing - no working

--- physics_db/urls.py
+++ physics_db/urls.py
@@ -22,6 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),
 ]
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
