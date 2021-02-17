fcbc5f258f4764ecd857b79c7c19ad317687e479
Pranay Begwani <14PBegwani@patesgs.org>
2021-02-17 12:58:20 +0000

    creating model/tables & link tbl to store pract

--- inventory/models.py
+++ inventory/models.py
@@ -16,11 +16,24 @@ from django.db import models
 
 # the count of the field below must be equal to the count of columns in db table - therefore deleting quantity_ne
 class Inventory_Equipment(models.Model):
-    id = models.IntegerField(primary_key=True, null=False, blank=True)
-    name = models.CharField(max_length=255, blank=True, null=True, default="Name this Equipment") # might have to divide this into name from frop down and new name
-    total_quantity = models.IntegerField(null=True, blank=True) # qty to add an entirely new equipment - bottom of form
-    location = models.CharField(max_length=20, default='Physics Office', blank=True)
-    img_reference = models.ImageField(null=True, blank=True, upload_to='images_uploaded/', default='images_uploaded/default.jpg')
+    #id = models.IntegerField(primary_key=True, null=False, blank=True)
+    name = models.CharField(
+        max_length=255, 
+        blank=True, 
+        null=True, 
+        default="Name this Equipment") # might have to divide this into name from frop down and new name
+    total_quantity = models.IntegerField(
+        null=True, 
+        blank=True) # qty to add an entirely new equipment - bottom of form
+    location = models.CharField(
+        max_length=20, 
+        default='Physics Office', 
+        blank=True)
+    img_reference = models.ImageField(
+        null=True, 
+        blank=True, 
+        upload_to='images_uploaded/', 
+        default='images_uploaded/default.jpg')
 
     class Meta:
         db_table="inventory"
@@ -30,6 +43,46 @@ class Inventory_Equipment(models.Model):
 
     #add validation functions
 
+class Practical(models.Model):
+    practical_name = models.CharField(
+        max_length=30, 
+        blank=True)
+    equipment_needed = models.ManyToManyField(
+        'Inventory_Equipment', 
+        blank=True, 
+        through='Practical_Equipment_Needed')
+    
+    class Meta:
+        db_table="practical"
+
+    def __str__(self):
+        return self.practical_name
+
+class Practical_Equipment_Needed(models.Model):
+    equipment_needed = models.ForeignKey(
+        'Inventory_Equipment', 
+        on_delete=models.CASCADE)
+    practical = models.ForeignKey(
+        'Practical', 
+        on_delete=models.CASCADE)
+    equipment_quantity = models.IntegerField(
+        default=0)
+
+    class Meta:
+        db_table="practical_equipment_needed"
+
+    def __str__(self):
+        return self.practical.name
+
+
+
+
+
+# class Practical(models.Model):
+#     id = models.IntegerField(primary_key=True, null=False, blank=True)
+#     practical_name = models.CharField(max_length=255, blank=True, null=True, default='Unnamed Practical')
+#     equipment_needed = models.ManyToManyField(Inventory_Equipment, )    
+
 # Need to figure out auto increase id
 # Need to figure out image upload to db
 # Show table when changes made
@@ -37,9 +90,6 @@ class Inventory_Equipment(models.Model):
 
 # less modularizing the app - making it complicated - add all tables in here and link them properly - one by one though
 # first complete inventory
-
-# class Practical(models.Model):
-#     pass
 
 # class Lessons(models.Model):
 #     pass
