8ca46e420fe9904aca534aa58099dccad36da013
Pranay Begwani <14PBegwani@patesgs.org>
2021-03-07 11:10:16 +0000

    Creating & registering new models for bookings

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from inventory.models import Inventory_Equipment, Practical, Practical_Equipment_Needed
+from inventory.models import Inventory_Equipment, Practical, Practical_Equipment_Needed, Room, Staff, Period, Lesson
 
 # Register your models here.
 
 admin.site.register(Inventory_Equipment)
 admin.site.register(Practical)
 admin.site.register(Practical_Equipment_Needed)
+admin.site.register(Room)
+admin.site.register(Staff)
+admin.site.register(Period)
+admin.site.register(Lesson)
